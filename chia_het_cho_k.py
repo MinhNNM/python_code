083c08367640889af221088b06af38ef64636f08
MinhNNM <91815119+MinhNNM@users.noreply.github.com>
2022-01-10 22:46:54 +0700

    Update chia_het_cho_k.py

--- chia_het_cho_k.py
+++ chia_het_cho_k.py
@@ -1,3 +1,4 @@
+#C1
 from math import *
 a = [int(i) for i in input().split()]
 b = a[2] - a[0]
@@ -15,3 +16,24 @@ if len(kq) == 0 or kq[0]==0:
     print(-1)
 else:
     print(' '.join([str(i) for i in kq]))
+    
+ 
+# C2
+a=[int(i) for i in input().split()]
+x=a[0]; k=a[1]; n=a[2]
+if x>=n or k>n:
+    print("-1")
+else:
+    check=0
+    if x>k:
+        tmp=k-x%k
+    elif x==k:
+        tmp=k
+    else:
+        tmp=k-x
+    while tmp+x<=n:
+        print(tmp, end=' ')
+        tmp+=k
+        check=1
+    if check==0:
+        print('-1')
